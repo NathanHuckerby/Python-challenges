0346c083b2356b54dea81bfc0b8d63950827efc1
Nathan Huckerby <nathanhuckerby20@gmail.com>
2023-09-18 10:14:20 +0100

    Python Challenges

---
+++ Programming challenges.py
@@ -0,0 +1,13 @@
+# Area of a Triangle
+base = int(input("Please enter the base value of the triangle: "))
+height = int(input("Please enter the height value of the triangle: "))
+
+TriangleArea = base*height/2
+print("The area of the triangle is: ",TriangleArea)
+
+# Power Calculator
+voltage = int(input("Please enter the voltage: "))
+current = int(input("Please enter the current: "))
+
+power = voltage*current
+print("The calculated power is: ",power)
