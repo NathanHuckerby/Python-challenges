affa4f8d7403f3f9c5241f46438e7a47bb0a9e88
Nathan Huckerby <nathanhuckerby20@gmail.com>
2023-09-18 10:17:22 +0100

    Update Programming challenges.py

--- Programming challenges.py
+++ Programming challenges.py
@@ -6,8 +6,30 @@ TriangleArea = base*height/2
 print("The area of the triangle is: ",TriangleArea)
 
 # Power Calculator
+print("\n")
 voltage = int(input("Please enter the voltage: "))
 current = int(input("Please enter the current: "))
 
 power = voltage*current
 print("The calculated power is: ",power)
+
+# Return the sum of two numbersh
+print("\n")
+number_1 = int(input("Please enter the first number: "))
+number_2 = int(input("Please input the second number: "))
+if number_1 % 7 == 0:
+    number_1/7
+else:
+    if number_2 % 7 == 0:
+        number_2/7
+
+if number_1 & 11 == 0:
+    number_1/11
+else:
+    if number_2 & 11 == 0:
+        number_2/11
+    
+sumNumbers = number_1 + number_2
+print("The sum of the two numbers is: ",sumNumbers)
+
+
